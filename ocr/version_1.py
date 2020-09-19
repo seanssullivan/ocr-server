737133f8296e77c7ea692fdc5123370cf8cafeab
Sean Sullivan <dev@seanmedia.ca>
2020-09-19 12:29:30 -0600

    Refactored OCR into separate folder

---
+++ ocr/version_1.py
@@ -0,0 +1,62 @@
+import cv2
+from flask import Blueprint, redirect, request
+import numpy as np
+import pytesseract
+
+from . import settings
+
+
+def allowed(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in settings.ALLOWED_EXTENSIONS
+
+
+def preprocess(image):
+    """Convert image to greyscale and adjust brightness to facilitate OCR."""
+    greyscale_image = convert_to_greyscale(image)
+    adjusted_image = adjust_brightness(greyscale_image)
+    binary_image = convert_to_binary(adjusted_image)
+    return binary_image
+
+
+def convert_to_greyscale(image):
+    greyscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)[..., 2]
+    return greyscale_image
+
+
+def adjust_brightness(image):
+    adjustment = int((255 - np.median(image)) * 0.75)
+    adjusted_image = np.where((255 - image) < adjustment, 255, image + adjustment)
+    return adjusted_image
+
+
+def convert_to_binary(image):
+    blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+    threshold, binary_image = cv2.threshold(
+        blurred_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return binary_image
+
+
+
+v1 = Blueprint('v1', __name__, url_prefix='/v1')
+
+@v1.route('/', methods=['POST'])
+def upload_file():
+    # check if the post request has a file attached
+    if 'image-file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['image-file']
+
+    # if user does not select file, browser may submit an empty string
+    if file.filename == '':
+        return redirect(request.url)
+
+    # if file type is allowed, process the image for text
+    if file and allowed(file.filename):
+        data = np.frombuffer(file.read(), np.uint8)
+        raw_image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        
+        processed_image = preprocess(raw_image)
+        text = pytesseract.image_to_string(processed_image)
+        return text
--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,6 @@
-import cv2
-from flask import Flask, redirect, render_template, request
-import numpy as np
-import pytesseract
+from flask import Flask, render_template
 
-from . import settings
-from .processing import preprocess
-
+from ocr import v1
 
 def create_app():
     # create and configure the app
@@ -15,30 +10,5 @@ def create_app():
     def index(name=None):
         return render_template('index.html', name=name)
 
-    @app.route('/', methods=['POST'])
-    def upload_file():
-        # check if the post request has a file attached
-        if 'image-file' not in request.files:
-            return redirect(request.url)
-
-        file = request.files['image-file']
-
-        # if user does not select file, browser may submit an empty string
-        if file.filename == '':
-            return redirect(request.url)
-
-        # if file type is allowed, process the image for text
-        if file and allowed(file.filename):
-            data = np.frombuffer(file.read(), np.uint8)
-            raw_image = cv2.imdecode(data, cv2.IMREAD_COLOR)
-            
-            processed_image = preprocess(raw_image)
-            text = pytesseract.image_to_string(processed_image)
-            return text
-
+    app.register_blueprint(v1)
     return app
-
-
-def allowed(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in settings.ALLOWED_EXTENSIONS
